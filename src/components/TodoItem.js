ad8729212b9f63c82b7cc6d0ed140725bba82ceb
EC2 Default User <ec2-user@ip-172-31-1-252.ec2.internal>
2021-04-09 15:52:22 +0000

    Change Todo status

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,14 +1,27 @@
+import React, { useState } from 'react';
 /* 
   【TodoItemコンポーネント】
 　・Todoアイテムを表示する
 　・チェックボックスにチェックが入っているか管理する
 　・チェックボックスにチェックが入っているかアイテムをグレーアウトする
 */
-function TodoItem( { item } ) {
+function TodoItem( { item } )  {
+  const [isGrey, setGrey] = useState(false);
+  
+  const DefText = () => {return (<p>{item.text}</p>);};
+  
+  const GreyText = () => {return (<p  class = 'has-text-grey-light'>{item.text}</p>);};
+
   return (
     <label className="panel-block">
-      <input type="checkbox" />
-      {item.text}
+      <input type="checkbox" 
+        onClick= { () => {
+          setGrey(!isGrey)
+        }} 
+      />
+      
+      {isGrey ? <GreyText /> : <DefText /> }
+      
     </label>
   );
 }
