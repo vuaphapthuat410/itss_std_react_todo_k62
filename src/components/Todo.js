2fb8f0beebb6dd5d25977d8a388bfadc0aa7a502
EC2 Default User <ec2-user@ip-172-31-1-252.ec2.internal>
2021-04-11 09:28:48 +0000

    Add Tabs

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 /* 
   【Todoのデータ構成】
@@ -22,17 +24,50 @@ function Todo() {
   const [items, putItems] = React.useState([
       /* テストコード 開始 */
     { key: getKey(), text: '日本語の宿題', done: false },
-    { key: getKey(), text: 'reactを勉強する', done: false },
+    { key: getKey(), text: 'reactを勉強する', done: true },
     { key: getKey(), text: '明日の準備をする', done: false },
     /* テストコード 終了 */
   ]);
   
   const addWord = (evt) => {
-      putItems(items => [...items, {key : getKey(), text: newText, done: false}])
-      setNewText("")
+        putItems(items => [...items, {key : getKey(), text: newText, done: false}])
+        setNewText("")
   };
   
+  /* save new text when change occurs*/
   const [newText, setNewText] = React.useState("");
+  
+  /* save number of item when items changed*/
+  const [quantity, setQuantity] = React.useState(items.length);
+  
+  const [key, setKey] = useState('home');
+  /* function that will let child change state of parents*/
+  function updateStatus(key, bool) {
+    var tmp = [...items]
+    for(var idx = 0; idx < items.length; ++idx) {
+      if(tmp[idx].key == key) {
+        tmp[idx].done = bool;
+        break;
+      }
+    }
+    putItems(tmp)
+  }
+  /* show item list based on conditions , this make me so confused why if there doesn't have this one, 
+  if u bring all the below code to where this function called, and yes the containers changes but it behaves abnormally
+  if you switch between tabs*/
+  const ShowList = ({arr}) => {
+    setQuantity(arr.length)
+    return (
+      <div>
+        {arr.map((item) => (
+          <TodoItem
+            item = {item}
+            onComplete = {updateStatus}
+          />
+        ))}
+      </div>
+    )
+  }
   
   return (
     <div className="panel">
@@ -40,19 +75,36 @@ function Todo() {
         ITSS ToDoアプリ
       </div>
       <div style = {{display: "flex", width: '20%'}}>
-        <input class = "input" type = "text" value={newText} onChange={e => setNewText(e.target
-        .value)}/>
-        <button type = "button" onClick={addWord}>Add</button>
-      </div>
-      {items.map(item => (
-        <TodoItem
-          key = {item.key}
-          item = {item}
+        <input class = "input" type = "text" placeholder ="Todoを入力してください" value={newText} 
+          onChange = {e => setNewText(e.target.value)}
+          onKeyPress={evt => {
+            if (evt.key === 'Enter') addWord() 
+          }}
         />
-      ))}
-      <div className="panel-block">
-        {items.length} items
       </div>
+      
+      <Tabs defaultIndex={0} >
+        <TabList>
+          <Tab>すべて</Tab>
+          <Tab>未完了</Tab>
+          <Tab>完了済み</Tab>
+        </TabList>
+          <TabPanel>
+            <ShowList arr = {items} /> 
+          </TabPanel>
+          <TabPanel>
+            <ShowList arr = {items.filter(item => item.done == false)} /> 
+          </TabPanel>
+          <TabPanel>
+            <ShowList arr = {items.filter(item => item.done == true)} /> 
+          </TabPanel>
+      </Tabs>
+      
+      
+      <div className="panel-block">
+        {quantity} items
+      </div>
+      
     </div>
   );
 }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,18 +5,19 @@ import React, { useState } from 'react';
 　・チェックボックスにチェックが入っているか管理する
 　・チェックボックスにチェックが入っているかアイテムをグレーアウトする
 */
-function TodoItem( { item } )  {
-  const [isGrey, setGrey] = useState(false);
+function TodoItem( props )  {
+  const [isGrey, setGrey] = useState(props.item.done);
   
-  const DefText = () => {return (<p>{item.text}</p>);};
+  const DefText = () => {return (<p>{props.item.text}</p>);};
   
-  const GreyText = () => {return (<p  class = 'has-text-grey-light'>{item.text}</p>);};
+  const GreyText = () => {return (<p  class = 'has-text-grey-light'>{props.item.text}</p>);};
 
   return (
     <label className="panel-block">
-      <input type="checkbox" 
+      <input type="checkbox" defaultChecked = {props.item.done}
         onClick= { () => {
           setGrey(!isGrey)
+          props.onComplete(props.item.key, !isGrey) /* cuz setGrey doesn't change isGrey until onClick func end */
         }} 
       />
       
