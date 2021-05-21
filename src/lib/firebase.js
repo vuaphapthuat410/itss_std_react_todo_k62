9443269813657dd82e1fc29a57e87878c8725838
EC2 Default User <ec2-user@ip-172-31-1-252.ec2.internal>
2021-05-21 07:22:00 +0000

    Use Firebase

---
+++ src/lib/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBz3mVL_ORNqKpLDk_eDmUtiTYItpYHA_Q",
+    authDomain: "fir-sample-2263c.firebaseapp.com",
+    projectId: "fir-sample-2263c",
+    storageBucket: "fir-sample-2263c.appspot.com",
+    messagingSenderId: "913703448881",
+    appId: "1:913703448881:web:efeecfac94f3811e038010"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
